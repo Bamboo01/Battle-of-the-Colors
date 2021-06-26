4edf2ddb1c5ab20f0f24b804c6092a6216f64328
K0019 <39440047+K0019@users.noreply.github.com>
2021-06-26 23:29:05 +0800

    Fix Merge Conflicts with Main

--- Assets/Scripts/NetworkedCharacter/InputActions/NetworkCharacter_Actions.cs
+++ Assets/Scripts/NetworkedCharacter/InputActions/NetworkCharacter_Actions.cs
@@ -11,7 +11,8 @@ namespace CSZZGame.Character
         MOVE,
         STEALTH,
         SHOOT,
-        SKILL
+        SKILL,
+        LAUNCH_STRATEGEM
     }
 
     public interface IAction_Character
@@ -88,5 +89,17 @@ namespace CSZZGame.Character
         {
             this.skillID = skillID;
         }
+    }
+
+    public class Action_Character_LaunchStrategem : IAction_Character
+    {
+        public Action_Character_Type identifier => Action_Character_Type.LAUNCH_STRATEGEM;
+        public bool IsActionAllowed(FSMState_Character_Type stateType) => stateType switch
+        {
+            FSMState_Character_Type.NORMAL => true,
+            FSMState_Character_Type.STEALTH_NORMAL => false,
+            FSMState_Character_Type.STEALTH_CLIMB => false,
+            _ => throw new System.NotImplementedException()
+        };
     }
 }
